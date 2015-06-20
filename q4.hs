71a238b5b074529f51f0ce5404b56bad65f1a7f0
Felix Chapman <aelred717@gmail.com>
2015-06-20 20:24:48 +0100

    Speed up question 4 solution

--- q4.hs
+++ q4.hs
@@ -1,6 +1,6 @@
 isPalindrome x = s == reverse s
     where s = show x
 
-palindromes = [x * y | x <- [100..999], y <- [100..999], isPalindrome (x * y)]
+palindromes = [x * y | x <- [100..999], y <- [x..999], isPalindrome (x * y)]
 
 main = print $ maximum palindromes
