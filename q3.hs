6d4421c45a26b43b7590883a307cd026efdac14e
Felix Chapman <aelred717@gmail.com>
2015-06-20 20:15:52 +0100

    Add solution to question 3

---
+++ q3.hs
@@ -0,0 +1,10 @@
+smallestFactor x = head $ filter ((==0) . (mod x)) [2..x]
+
+largestFactor x = x `quot` smallestFactor x
+
+largestPrimeFactor x 
+    | largest == 1 = x
+    | otherwise    = largestPrimeFactor largest
+    where largest = largestFactor x
+
+main = print $ largestPrimeFactor 600851475143
